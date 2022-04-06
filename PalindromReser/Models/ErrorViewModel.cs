ba3ed8dcc3c85562a4f323121824f61618cd86eb
alexnaev <102225427+alexnaev@users.noreply.github.com>
2022-04-06 19:57:54 +0200

    Initial commit

---
+++ PalindromReser/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PalindromReser.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
